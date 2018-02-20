06f4959903a8533fa5edd20e0177aa4125be628c
Karolis Stasaitis <stkarolis@gmail.com>
2018-02-21 00:00:15 +0100

    dokku and rustup

--- dependencies/shared-deps.sh
+++ dependencies/shared-deps.sh
@@ -22,7 +22,13 @@ directory_absent? "$HOME/.nvm" && curl -o- https://raw.githubusercontent.com/cre
 # rvm
 directory_absent? "$HOME/.rvm" && curl -sSL https://get.rvm.io | bash -s stable
 
+# rustup
+directory_absent? "$HOME/.cargo/bin" && curl https://sh.rustup.rs -sSf | sh
+
 # vim-plug
 file_absent? "$HOME/.vim/autoload/plug.vim" && curl -fLo "$HOME/.vim/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim && vim +PlugInstall +qall
+
+# dokku-client
+directory_absent? "$HOME/.dokku/bin" && git clone git@github.com:dokku/dokku.git "$HOME/.dokku"
 
 exit 0
--- zsh/utils.zsh
+++ zsh/utils.zsh
@@ -1,7 +1,5 @@
 # helper functions
 
-alias ssh2chip="ssh chip@carlossless-chip.local"
-
 ts2date () {
   perl -e "print scalar localtime($1 / 1000)"
 }
